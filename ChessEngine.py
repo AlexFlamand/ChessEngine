d2a1a02eb88d725ef917a990af348c26bc2ef000
Alex Flamand <pafword@gmail.com>
2021-08-16 05:13:28 -0400

    Added code from Part 2 of the tutorial series
    Adds movement functionality to the drawn pieces on the gameboard.

--- ChessEngine.py
+++ ChessEngine.py
@@ -21,3 +21,30 @@ class GameState:
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"], ]
         self.whiteToMove = True
         self.moveLog = []
+
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)  # Logs the move to undo the move later or keep track
+        self.whiteToMove = not self.whiteToMove  # Swaps players
+
+
+class Move:
+    # Maps keys to values
+    # Key : value
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+        # This can be modified to output real chess notation
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
--- ChessMain.py
+++ ChessMain.py
@@ -4,12 +4,14 @@ object.
 """
 
 import pygame as p
+from pygame import MOUSEBUTTONDOWN
+
 from Chess import ChessEngine
 
-WIDTH = HEIGHT = 512  # 400 is another option/
-DIMENSION = 8  # Dimensions of the chessboard/
+WIDTH = HEIGHT = 512  # 400 is another option
+DIMENSION = 8  # Dimensions of the chessboard
 SQ_SIZE = HEIGHT // DIMENSION
-MAX_FPS = 15  # For animations later on/
+MAX_FPS = 15  # For animations later on
 IMAGES = {}
 
 '''
@@ -35,12 +37,31 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
-    loadImages()  # Only do this once, before the while loop/
+    loadImages()  # Only do this once, before the while loop
     running = True
+    sqSelected = ()  # No square is selected initially, keep track of the last click of the user (tuple: (row, col))
+    playerClicks = []  # Keeps rack of player clicks (two tuples: [(6, 4), (4, 4)])
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()  # (X, Y) Location of the mouse
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
+                if sqSelected == (row, col):  # If user clicked the same square twice to undo action
+                    sqSelected = ()  # Deselect
+                    playerClicks = []  # Clear player clicks
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)  # Append for both 1st and 2nd clicks
+                if len(playerClicks) == 2:  # After 2nd click
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    #  print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = ()  # Resets user inputs
+                    playerClicks = []
+
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
@@ -52,9 +73,9 @@ Responsible for all graphics within the current game state.
 
 
 def drawGameState(screen, gs):
-    drawBoard(screen)  # Draws the squares on the board/
-    # Add feature for piece highlighting or move suggestions later/
-    drawPieces(screen, gs.board)  # Draws pieces on top of those squares/
+    drawBoard(screen)  # Draws the squares on the board
+    # Add feature for piece highlighting or move suggestions later
+    drawPieces(screen, gs.board)  # Draws pieces on top of those squares
 
 
 '''
@@ -79,7 +100,7 @@ def drawPieces(screen, board):
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             piece = board[r][c]
-            if piece != "--":  # Not an empty square/
+            if piece != "--":  # Not an empty square
                 screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
